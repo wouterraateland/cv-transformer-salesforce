1073eadd69d3aca118b93a987f5eb0a6fc8ef9f9
Wouter Raateland <wouterraateland@gmail.com>
2025-03-10 19:41:16 +0100

    Add api key check

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-## How Do You Plan to Deploy Your Changes?
+## Installation
 
-Do you want to deploy a set of changes, or create a self-contained application? Choose a [development model](https://developer.salesforce.com/tools/vscode/en/user-guide/development-models).
+- Install `sf` CLI
+- Install `openjdk@21`
 
-## Configure Your Salesforce DX Project
+## Deploying a new version
 
-The `sfdx-project.json` file contains useful configuration information for your project. See [Salesforce DX Project Configuration](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_ws_config.htm) in the _Salesforce DX Developer Guide_ for details about this file.
-
-## Read All About It
-
-- [Salesforce Extensions Documentation](https://developer.salesforce.com/tools/vscode/)
-- [Salesforce CLI Setup Guide](https://developer.salesforce.com/docs/atlas.en-us.sfdx_setup.meta/sfdx_setup/sfdx_setup_intro.htm)
-- [Salesforce DX Developer Guide](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_intro.htm)
-- [Salesforce CLI Command Reference](https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference.htm)
+- `sf project deploy start`
+- `sf project deploy finish`s
--- force-app/main/default/lwc/contactActions/contactActions.js
+++ force-app/main/default/lwc/contactActions/contactActions.js
@@ -1,25 +1,54 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import sendDataToApi from "@salesforce/apex/ExternalApiHandler.sendDataToApi";
+import candidateCreate from "@salesforce/apex/CVTransformerApi.candidateCreate";
+import configurationGet from "@salesforce/apex/CVTransformerApi.configurationGet";
 
 export default class ContactActions extends LightningElement {
   @api recordId;
+  api_key;
+  candidate_id;
+  error;
+  organization_id;
 
-  handleApiCall() {
-    sendDataToApi({ contactId: this.recordId })
+  @wire(configurationGet, { contactId: "$recordId" })
+  wiredData({ error, data }) {
+    console.log(error, data);
+    if (data) {
+      this.api_key = data.api_key__c;
+      this.organization_id = data.name;
+      this.error = null;
+    } else if (error) {
+      this.error = error;
+      this.api_key = null;
+      this.organization_id = null;
+    }
+  }
+
+  apiKeySave = (event) => {
+    // Form event
+    event.preventDefault();
+    console.log(event);
+  };
+
+  handleApiCall = () => {
+    candidateCreate({ contactId: this.recordId })
       .then(() => {
-        this.showToast("Success", "API triggered successfully", "success");
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "API triggered successfully",
+            variant: "success"
+          })
+        );
       })
       .catch((error) => {
-        this.showToast("Error", error.body.message, "error");
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error",
+            message: error.body.message,
+            variant: "error"
+          })
+        );
       });
-  }
-
-  handleUploadFinished() {
-    this.showToast("Success", "File uploaded successfully", "success");
-  }
-
-  showToast(title, message, variant) {
-    this.dispatchEvent(new ShowToastEvent({ title, message, variant }));
-  }
+  };
 }
